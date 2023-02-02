0e3052acbe3cbb8a61977863446f85a2b240d5d6
eihabahmed <eihabahmed@gmail.com>
2023-02-02 19:31:06 +0200

    Recover password with NGRX state management (Part 11)

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -2,26 +2,31 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { Store, StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from 'src/app/app-routing.module';
+import { AppState } from 'src/store/AppState';
 
 import { LoginPage } from './login.page';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
-  let router: Router
+  let router: Router;
+  let store: Store<AppState>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginPage ],
       imports: [
         IonicModule.forRoot(),
+        StoreModule.forRoot([]),
         AppRoutingModule,
         ReactiveFormsModule
       ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
+    store = TestBed.get(Store);
     router = TestBed.get(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
